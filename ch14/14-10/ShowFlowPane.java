c105fc8c4956608c2b34e54711b2ec3efe936be1
ZhengkaiXu <695378091@qq.com>
2025-01-25 17:33:11 +0800

    14-11

---
+++ ch14/14-10/ShowFlowPane.java
@@ -0,0 +1,34 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class ShowFlowPane extends Application {
+    @Override // Override the start method in the Application class
+    public void start(Stage primaryStage) {
+        FlowPane pane = new FlowPane();
+        pane.setPadding(new Insets(11, 12, 13, 14));
+        pane.setHgap(5);
+        pane.setVgap(5);
+
+        // Place nodes in the pane
+        pane.getChildren().addAll(new Label("First Name:"), new TextField(), new Label("MI:"));
+        TextField tfMi = new TextField();
+        tfMi.setPrefColumnCount(1);
+        pane.getChildren().addAll(tfMi, new Label("Last Name:"), new TextField());
+
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(pane, 200, 200);
+        primaryStage.setTitle("ShowFlowPane"); // Set the stage title
+        primaryStage.setScene(scene);
+        primaryStage.show(); // Display the stage
+
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
