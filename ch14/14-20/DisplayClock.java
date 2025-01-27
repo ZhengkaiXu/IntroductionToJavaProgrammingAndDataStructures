52f9f838f0cb07add4fed59f936a081ea29ec644
ZhengkaiXu <695378091@qq.com>
2025-01-27 23:52:33 +0800

    14-20

---
+++ ch14/14-20/DisplayClock.java
@@ -0,0 +1,6 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
