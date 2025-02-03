400e44a7d3df7b8737228676d6c0efbb5fdb60fa
ZhengkaiXu <695378091@qq.com>
2025-02-03 18:45:31 +0800

    15-2

---
+++ ch15/15-2/ControlCircleWithoutEventHandling.java
@@ -0,0 +1,44 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class ControlCircleWithoutEventHandling extends Application {
+    @Override // Override the start method in the Application class
+    public void start(Stage primaryStage) {
+        StackPane pane = new StackPane();
+        Circle circle = new Circle(50);
+        circle.setFill(Color.WHITE);
+        circle.setStroke(Color.BLACK);
+        pane.getChildren().add(circle);
+
+        HBox hBox = new HBox();
+        hBox.setSpacing(10);
+        hBox.setAlignment(Pos.CENTER);
+        Button btEnlarge = new Button("Enlarge");
+        Button btShrink = new Button("Shrink");
+        hBox.getChildren().add(btEnlarge);
+        hBox.getChildren().add(btShrink);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(pane);
+        borderPane.setBottom(hBox);
+        BorderPane.setAlignment(hBox, Pos.CENTER);
+
+        // Create a scene and place and place it the stage
+        Scene scene = new Scene(borderPane, 200, 150);
+        primaryStage.setTitle("ControlCircle"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
