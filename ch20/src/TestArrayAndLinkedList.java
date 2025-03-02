4ba4848b1d6b8479c5da38b8daaa65f8cf88c21d
ZhengkaiXu <695378091@qq.com>
2025-03-02 18:19:21 +0800

    20250302

---
+++ ch20/src/TestArrayAndLinkedList.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class TestArrayAndLinkedList {
+    public static void main(String[] args) {
+        List<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1); // 1 is autoboxed to an Integer object
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(1);
+        arrayList.add(4);
+    }
+}
